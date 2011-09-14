b3b635e536cc797ea8f52c9573ed0d5bcb336e48
nep <poopsmith@lavabit.com>
2011-09-14 16:04:00 +0200

    Fix some stuff in 'camera.py'.

--- resources/camera.py
+++ resources/camera.py
@@ -3,7 +3,19 @@
 # screen's width and height.
 #
 # When you want to calculate the offset of everything call calculate_position()
-# with the player/character's x and y coordinates
+# with the player/character's x and y coordinates and it returns the camera's
+# coordinates. To use those the best idea (probably) is to subtract them from
+# whatever coordinates you already have. Kinda like this:
+#
+# cam = Camera( map_width, map_height, screen_width, screen_height )
+# camera_x, camera_y = cam.calculate_position( player_x, player_y )
+# screen.blit( tile_x - camera_x, tile_y - camera_y )
+#
+# TODOs:
+#
+# - When the map is smaller than the screen, the map gets drawn on the
+# top-left corner without any nice padding. I'm thinking of positioning the
+# camera on the center of the map in this case.
 #
 class Camera(  ):
     def __init__( self, map_width, map_height, screen_width, screen_height, tile_size = 32 ):
@@ -16,15 +28,13 @@ class Camera(  ):
         if x < 0:
         	# lock left side
         	x = 0
-        elif x > ( self.mw * self.ts ):
+        elif x > ( self.mw * self.ts ) - ( self.sw ):
         	# lock right side
         	x = ( self.mw * self.ts ) - ( self.sw )
         if y < 0:
         	# lock top
         	y = 0
-        elif x > ( self.mh * self.ts ):
+        elif x > ( self.mh * self.ts ) - ( self.sh ):
         	# lock bottom
         	y = ( self.mh * self.ts ) - ( self.sh )
         return( x, y )
-        # ugly stuff: when the map is smaller than the screen, the map gets
-        # drawn on the top-left corner without any nice padding
