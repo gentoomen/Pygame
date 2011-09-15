0884b76c8852888b6ef84e5de501a2eddd7f8ce3
nep <poopsmith@lavabit.com>
2011-09-15 16:34:08 +0200

    Add lava tile.

--- resources/blocks.py
+++ resources/blocks.py
@@ -2,7 +2,7 @@
 blocks = {
     "0" : {
         "solid" : False, # I really don't think 'space' should be solid.
-        "transparent" : 1, # I guess...
+        "transparent" : 255, # I guess...
         "material" : "space.png",
         "damage" : 0,
         "health" : 1000000 # I don't know about this man.
@@ -20,5 +20,12 @@ blocks = {
         "material" : "stone.jpg",
         "damage" : 0,
         "health" : 40
+    },
+    "3" : {
+        "solid" : False,
+        "transparent" : 127,
+        "material" : "lava.jpg", # this is actually 'cloud.jpg' with altered coloring
+        "damage" : 1,
+        "health" : 1000000
     }
 }
